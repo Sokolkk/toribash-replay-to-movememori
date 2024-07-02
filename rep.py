eacd173f61bad4429a92e257b6362968ed839e93
Sokolkk <47243051+Sokolkk@users.noreply.github.com>
2024-07-02 20:20:00 +0500

    Update rep.py

--- rep.py
+++ rep.py
@@ -5,7 +5,7 @@ def extract_data(input_file, output_file, move_name, joint):
     try:
         with open(input_file, 'r') as infile, open(output_file, 'a') as outfile:
             # Записываем строки NAME и MOD
-            outfile.write(f"NAME mushu {move_name}\n")
+            outfile.write(f"NAME {move_name}\n")
             outfile.write("MOD mushu\n")
 
             turn_number = 1
@@ -19,11 +19,13 @@ def extract_data(input_file, output_file, move_name, joint):
         print(f"Error: The file '{input_file}' was not found in the current directory.")
 
 # Запрашиваем имя входного файла, название приема и игрока у пользователя
-input_filename = input("Имя файла: ") + '.rpl'
+input_filename = input("Имя файла: ") 
 move_name = input("Название приема: ")
 output_filename = r"C:\Program Files (x86)\Steam\steamapps\common\Toribash\data\script\system\movememory.mm"
-joint = 'JOINT ' + input("игрок 1 или 0: ") + ';'
-
+player = input("игрок 1 или 0 (по умолчанию 0): ")
+if not player:
+    player = '0'
+joint = 'JOINT ' + player + ';'
 # Вызываем функцию
 extract_data(input_filename, output_filename, move_name, joint)
 
