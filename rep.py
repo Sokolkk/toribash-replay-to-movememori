33aeb6da493c14299964c60dfa4b008fd0670479
Sokolkk <47243051+Sokolkk@users.noreply.github.com>
2024-07-02 15:31:06 +0500

    #торибащ сохранение движений из реплеев

---
+++ rep.py
@@ -0,0 +1,33 @@
+#торибащ сохранение движений из реплеев
+import re
+
+def extract_data(input_file, output_file, move_name, joint):
+    try:
+        with open(input_file, 'r') as infile, open(output_file, 'a') as outfile:
+            # Записываем строки NAME и MOD
+            outfile.write(f"NAME mushu {move_name}\n")
+            outfile.write("MOD mushu\n")
+
+            turn_number = 1
+            for line in infile:
+                match = re.match(rf'{joint}\s*(.*)', line)
+                if match:
+                    data = match.group(1).strip()
+                    outfile.write(f"TURN {turn_number}; {data}\n")
+                    turn_number += 1
+    except FileNotFoundError:
+        print(f"Error: The file '{input_file}' was not found in the current directory.")
+
+# Запрашиваем имя входного файла, название приема и игрока у пользователя
+input_filename = input("Имя файла: ") + '.rpl'
+move_name = input("Название приема: ")
+output_filename = r"C:\Program Files (x86)\Steam\steamapps\common\Toribash\data\script\system\movememory.mm"
+joint = 'JOINT ' + input("игрок 1 или 0: ") + ';'
+
+# Вызываем функцию
+extract_data(input_filename, output_filename, move_name, joint)
+
+
+
+
+
